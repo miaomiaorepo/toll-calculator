d535c7a0b40da6f9ba7d1fa67c4a52e2412381ca
Shuyao.X <shuyao1201@gmail.com>
2024-01-26 14:26:37 -0500

    add route

--- js/map-init.js
+++ js/map-init.js
@@ -1,10 +1,36 @@
 function initMap() {
-    var map = L.map('map').setView([32.7767, -96.7970], 10); // Dallas County Coordinates
+
+    fetch('routes.json')
+        .then(response => response.json())
+        .then(data => {
+            const routeData = data.route[0]; 
+            setupMapAndRoute(routeData);
+        })
+        .catch(error => console.error('Error loading the JSON file:', error));
+}
+
+function setupMapAndRoute(routeData) {
+    var origin = routeData.origin;
+    var destination = routeData.destination;
+    var polylinePoints = routeData.polyline;
+
+    var map = L.map('map').fitBounds([
+        [origin.origin_lat, origin.origin_lng],
+        [destination.destination_lat, destination.destination_lng]
+    ]);
+
+
+    //var map = L.map('map').setView([32.7767, -96.7970], 10); // Dallas County Coordinates
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '© OpenStreetMap contributors'
     }).addTo(map);
+
+    
+    var polyline = L.polyline(polylinePoints.map(point => point.reverse()), {color: 'blue'}).addTo(map);
+    var originMarker = L.marker([origin.origin_lat, origin.origin_lng]).addTo(map);
+    var destinationMarker = L.marker([destination.destination_lat, destination.destination_lng]).addTo(map);
 }
 
 document.addEventListener('DOMContentLoaded', initMap);
